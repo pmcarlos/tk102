fd273ff68350b5e487e368a9fc6329001b389b19
pmcarlos <carlos@passaromx.com>
2018-06-07 14:09:51 -0500

    test

---
+++ app.js
@@ -0,0 +1,52 @@
+/*
+  Start with:  node example.js
+  Telnet to:   telnet 127.0.0.1 1337
+  Copy/paste:  1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123
+*/
+
+var tk102 = require ('./tk102');
+var net = require ('net');
+
+//var gps = '1203292316,0031698765432,GPRMC,211657.000,A,5213.0247,N,00516.7757,E,0.00,273.30,290312,,,A*62,F,imei:123456789012345,123';
+
+
+
+// report only track event to console
+tk102.on ('track', function (gps) {
+  console.log ('\nIncoming GPS data:\n');
+  console.log (gps);
+  console.log('\n\n');
+});
+
+// wait for server to be ready
+tk102.on ('listening', function (lst) {
+  var client;
+
+  console.log ('TK102 server is ready');
+
+  // Send data with telnet
+  client = net.connect (lst.port, function () {
+    console.log ('Connected to TK102 server');
+    console.log ('Sending GPS data string for processing');
+
+    //client.write (gps + '\r\n');
+    client.end ();
+
+    console.log ('CTRL+C to exit');
+  });
+});
+
+// tk102.on ('connection', function (socket) {
+//   console.log ('Connection from '+ socket.remoteAddress);
+// });
+
+
+tk102.on ('fail', function (err) {
+  console.log (err);
+});
+
+
+// start server
+tk102.createServer ({
+  port: 1337
+});
