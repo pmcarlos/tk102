b189878cba99f9cbae56e73d21f3f301e029f761
pmcarlos <carlos@passaromx.com>
2018-09-27 20:09:24 -0500

    A

--- tk102.js
+++ tk102.js
@@ -68,7 +68,7 @@ tk102.createServer = function (vars) {
       const ch_ = [...ch]
       const an = ch_[11]
       // console.log('ch_', ch_)
-      const year = `${ch_[67]} ${ch_[68]}`
+      const year = `${ch_[67].toString('hex')} ${ch_[68].toString('hex')}`
       let buffer
       let ack_buffer
       ch_.forEach(val => {
